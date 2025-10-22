ef18a28430f47ef7e7d369e33ebb106651633505
quickhorn <quickhorn@gmail.com>
2025-10-22 10:51:02 -0600

    Library now has extended functionality.
    A librarian exists and users exist. Only one librarian. Librarian checks in books. User checks out books.
    
    A bunch of inconsistent error messaging and functionality exist due to the requirements of the challenge.

--- 01_02b/library.js
+++ 01_02b/library.js
@@ -1,11 +1,11 @@
-    // JavaScript code​​​​​​‌‌‌​​‌‌‌​​​​​‌‌​‌‌​‌​‌‌‌‌ below
-    // Write your answer here, and then test your code.
-    // Your job is to implement the findLargest() method.
+// JavaScript code​​​​​​‌‌‌​​‌‌‌​​​​​‌‌​‌‌​‌​‌‌‌‌ below
+// Write your answer here, and then test your code.
+// Your job is to implement the findLargest() method.
 
-    // Change these boolean values to control whether you see
-    // the expected answer and/or hints.
-    const showExpectedResult = true;
-    const showHints = false;
+// Change these boolean values to control whether you see
+// the expected answer and/or hints.
+const showExpectedResult = true;
+const showHints = false;
 
     class Book {
     constructor(title, author, publicationYear) {
@@ -18,38 +18,38 @@
     getDetails() {
         return `Title: ${this.title}, Author: ${this.author}, Publication Year: ${this.publicationYear}, Status: ${this.status}`;
     }
+}
+
+
+/**
+ * Extend the Book class with Fiction and NonFiction classes.
+ */
+// Your code starts here.
+class Fiction extends Book{
+    constructor(title, author, publicationYear, genre){
+        super(title, author, publicationYear);
+        this.genre = genre;
     }
 
+    getDetails() {
+        return `${super.getDetails()}, Genre: ${this.genre}`;
+    }
+}
 
-    /**
-     * Extend the Book class with Fiction and NonFiction classes.
-     */
-    // Your code starts here.
-    class Fiction extends Book{
-        constructor(title, author, publicationYear, genre){
-            super(title, author, publicationYear);
-            this.genre = genre;
-        }
-
-        getDetails() {
-            return `${super.getDetails()}, Genre: ${this.genre}`;
-        }
+class NonFiction extends Book{
+    constructor(title, author, publicationYear, subject){
+        super(title, author, publicationYear);
+        this.subject = subject;
     }
 
-    class NonFiction extends Book{
-        constructor(title, author, publicationYear, subject){
-            super(title, author, publicationYear);
-            this.subject = subject;
-        }
-
-        getDetails() {
-            return `${super.getDetails()}, Subject: ${this.subject}`;
-        }
+    getDetails() {
+        return `${super.getDetails()}, Subject: ${this.subject}`;
     }
+}
 
-    // Your code ends here.
+// Your code ends here.
 
-    class Library {
+class Library {
     constructor() {
         this.books = [];
         this.users = [];
@@ -107,8 +107,107 @@
             return `Book titled "${title}" not found.`;
         }
     }
-    // Your code ends here.
-    
+
+    addUser(user){
+        this.users.push(user);
     }
 
-    export { Library, Book, Fiction, NonFiction };
+    findUser(name){
+        return this.users.find(u => u.name === name);
+    }
+
+    //this method is different from the instructions, because the test passes in a library user
+    //instead of a library user name.
+    setLibrarian(user){
+        if (this.librarian){
+            return "A librarian already exists."
+        } else if (user){
+            this.librarian = user;
+        } else {
+            return `No user set`;
+        }
+    }
+}
+
+class LibraryUser {
+    constructor(name) {
+        this.name = name;
+        this.borrowedBooks = [];
+        this.suspended = false;
+    }
+
+    checkAvailability(library, title){
+        let book = library.books.find(b => b.title === title);
+        if (book != null)  {
+            if (book.status === "available"){
+                return `Book is available: ${book.getDetails()}`
+            } else  {
+                return `"${title}" is currently on loan.`
+            }
+        } else {
+            return `Book titled "${title}" not found.`
+        }
+    }
+
+    loanBook(library, title){
+        if (this.suspended){
+            return `${this.name} is suspended and cannot borrow books.`
+        }
+        //I wish that findBook returned a book and a status instead of a string so we could reuse
+        //the find method in case we need to modify it later instead of having it spread through
+        //multiple methods.
+        let book = library.books.find(b => b.title === title);
+        if (book != null)  {
+            if (book.status === "available"){
+                this.borrowedBooks.push(book);
+                book.status = "borrowed";
+                return `${this.name} has borrowed the book "${title}" from ${library.librarian.name}.`
+            } else  {
+                return `"${title}" is currently on loan.`
+            }
+        } else {
+            return `Book titled "${title}" not found.`
+        }
+    }
+    
+}
+
+    class Librarian extends LibraryUser {
+    constructor(name) {
+        super(name);
+    }
+
+    returnBook(library, title, userName){
+        //man this is confusing, because findbooks returns strings and finduser returns objects. >:( )
+        let user = library.findUser(userName);
+        let book = library.books.find(b => b.title === title);
+        if (user && book){
+            let borrowedBook = user.borrowedBooks.find(b => b.title === title);
+            if (borrowedBook){
+                user.borrowedBooks.filter((book) => book.title !== title);
+                borrowedBook.status = "available";
+                return `${this.name} has checked in the book "${title}" from ${user.name}.`
+            } else {
+                return `"${title}" not found in user's borrowed books.`;
+            }
+        } else {
+            //wtf with this response. user or book? not helpful. Whatever Linkedinlive.
+            return "Book or user not found."
+        }
+    }
+
+    suspendUser(library, userName){
+        let user = library.findUser(userName);
+        if (user){
+            user.suspended = true;
+            return `${this.name} has suspended ${userName}.`
+        } else {
+            return 'User not found.' //again, inconsistent error, where in other places we return the string used to find the object, we don't here.
+        }
+    }
+      
+}
+
+
+
+export { Library, Book, Fiction, NonFiction, Librarian, LibraryUser };
--- 01_02b/libraryTest.js
+++ 01_02b/libraryTest.js
@@ -1,8 +1,7 @@
-import { Library, Fiction, NonFiction } from "./library.js";
-
+import { Library, Fiction, NonFiction, Librarian, LibraryUser } from "./library.js";
 // This is how your code will be called.
-// Your answer should be the largest value in the numbers array.
-// You can edit this code to try different testing cases.
+// You can edit the book info in the three constants
+// at the top to test with different books.
 
 const book01 = {
     title: "Odyssey",
@@ -27,27 +26,38 @@ const book01 = {
   
   // Create a library with books, a librarian, and users
   const library = new Library();
-  console.log(
-    library.addBook(
-      new Fiction(book01.title, book01.author, book01.pubYear, book01.genre)
-    )
+  library.addBook(
+    new Fiction(book01.title, book01.author, book01.pubYear, book01.genre)
   );
-  console.log(
-    library.addBook(
-      new Fiction(book02.title, book02.author, book02.pubYear, book02.genre)
-    )
+  library.addBook(
+    new Fiction(book02.title, book02.author, book02.pubYear, book02.genre)
   );
-  console.log(
-    library.addBook(
-      new NonFiction(book03.title, book03.author, book03.pubYear, book03.genre)
-    )
+  library.addBook(
+    new NonFiction(book03.title, book03.author, book03.pubYear, book03.genre)
   );
+  const librarian = new Librarian("Alice"); // only one librarian
+  library.setLibrarian(librarian); // set the librarian
+  const user001 = new LibraryUser("Simran");
+  library.addUser(user001);
+  const user002 = new LibraryUser("Maiken");
+  library.addUser(user002);
+  const user003 = new LibraryUser("Chris");
+  library.addUser(user003);
+  const librarian2 = new Librarian("Bob");
   
-  // Test capabilities of the library
+  // Test capabilities of different actors
   const result = () => {
-    console.log(library.findBook("Odyssey")); // Book titled "Odyssey" is in the library
-    console.log(library.findBook("Silo")); // Book titled "Silo" not found.
-    console.log(library.removeBook("Odyssey")); // Book "Odyssey" removed. There are now 2 books in the library.
+    console.log(library.setLibrarian(librarian2)); // A librarian already exists.
+    console.log(user001.checkAvailability(library, book01.title)); // Book is available: Title: Odyssey, Author: Homer, Publication Year: 1726, Status: available, Genre: Coming of Age
+    console.log(user001.checkAvailability(library, "Silo")); // Book titled "Silo" not found.
+    console.log(user001.loanBook(library, book01.title)); // Simran has borrowed the book "Odyssey" from Alice.
+    console.log(user003.loanBook(library, book01.title)); // "Odyssey" is currently on loan.
+    console.log(librarian.returnBook(library, book01.title, "Simran")); // Alice has checked in the book "Odyssey" from Simran.
+    console.log(user003.loanBook(library, book01.title)); // Chris has borrowed the book "Odyssey" from Alice
+    console.log(librarian.returnBook(library, book02.title, "Maiken")); // "The Yellow Wallpaper" not found in user's borrowed books.
+    console.log(librarian.suspendUser(library, "Maiken")); // Alice has suspended Maiken.
+    console.log(user002.loanBook(library, book02.title)); // Maiken is suspended and cannot borrow books.
+    console.log(user001.loanBook(library, "Annihilation")); // Book titled "Annihilation" not found.
   };
 
   result();
